ce7ca3d9504ced2fe6d7e8708ea71e235fd00703
Diego Beltran <diego.beltran@neofinancial.com>
2022-11-28 19:51:48 -0600

    point of entry call all necessary functions

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+import { getOctokitClient } from "./lib/octokit-client";
+import { getConfigData } from "./lib/config-data";
+import { getReviewersToAssign } from "./lib/reviewers";
+
+(async () => {
+  try {
+    const octokitClient = await getOctokitClient();
+    const configData = await getConfigData(octokitClient);
+    // const reviewersToAssign = await getReviewersToAssign(
+    //   octokitClient,
+    //   configData
+    // );
+    await getReviewersToAssign(octokitClient, configData);
+
+    // console.log("====================================");
+    // console.log(reviewersToAssign);
+    // console.log("====================================");
+  } catch (err) {
+    core.setFailed(`Action failed with error ${err}`);
+  }
+})();
