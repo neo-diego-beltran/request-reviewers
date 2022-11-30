fd3db3f9b865e992c83be0ed5820191b6fa32e27
Diego Beltran <diego.beltran@neofinancial.com>
2022-11-29 21:34:34 -0600

    added everything together and will be the first major release

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 import * as core from "@actions/core";
 
-import { getOctokitClient } from "./lib/octokit-client.js";
-import { getConfigData } from "./lib/config-data.js";
-import { getReviewersToAssign } from "./lib/reviewers.js";
+import {
+  getOctokitClient,
+  getConfigData,
+  getReviewersToAssign,
+  assignReviewers,
+} from "./lib/octokit-config-reviewers.js";
 
 (async () => {
   try {
     const octokitClient = getOctokitClient();
     const configData = await getConfigData(octokitClient);
+    const reviewersToAssign = await getReviewersToAssign(
+      octokitClient,
+      configData
+    );
 
-    console.log("====================================");
-    console.log(configData);
-    console.log("====================================");
-    // const reviewersToAssign = await getReviewersToAssign(
-    //   octokitClient,
-    //   configData
-    // );
-    await getReviewersToAssign(octokitClient, configData);
-
-    // console.log("====================================");
-    // console.log(reviewersToAssign);
-    // console.log("====================================");
+    await assignReviewers(octokitClient, reviewersToAssign);
   } catch (err) {
     core.setFailed(`Action failed with error ${err}`);
   }
